af7001be966ea03d9509e0ba483878025ec86aab
scotch <87593993+launchlabsdev@users.noreply.github.com>
2021-08-05 08:04:46 -0500

    Create MetaBase.py

---
+++ MetaBase.py
@@ -0,0 +1,65 @@
+"""https://docs.python.org/3/reference/datamodel.html#metaclasses"""
+
+ 
+
+import logging
+
+import sys
+
+ 
+
+logging.basicConfig(stream=sys.stdout)
+
+ 
+
+class MetaBase(type):
+
+    def __init__(cls, *args):
+
+        super().__init__(*args)
+
+ 
+
+        # Explicit name mangling
+
+        logger_attribute_name = '_' + cls.__name__ + '__logger'
+
+ 
+
+        # Logger name derived accounting for inheritance for the bonus marks
+
+        logger_name = '.'.join([c.__name__ for c in cls.mro()[-2::-1]])
+
+ 
+
+        setattr(cls, logger_attribute_name, logging.getLogger(logger_name))
+
+ 
+
+class Base(metaclass=MetaBase):
+
+    def __init__(self):
+
+        self.__logger.error('init base')
+
+ 
+
+    def func_base(self):
+
+        self.__logger.error('func base')
+
+ 
+
+class Parent(Base):
+
+    def func_parent(self):
+
+        self.__logger.error('func parent')
+
+ 
+
+p = Parent()
+
+p.func_base()
+
+p.func_parent()
